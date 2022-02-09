c6255f82eaf488d0d512ea26829e4e3f5ef8a233
Basil <sharpshot101@hotmail.co.uk>
2022-02-09 17:55:36 +0000

    map and state
    Added array mapping for dynamic table elements. Working on an onclick function for the record rows

--- src/Components/C-ExpTable.js
+++ src/Components/C-ExpTable.js
@@ -1,7 +1,5 @@
-
+import React, {useState} from 'react';
 import { char_Obj } from "../Reference Files/ObjectDefs"
-
-
 import { 
     Wrapper,
     Table,
@@ -11,10 +9,23 @@ import {
 
 export const ExpTable = () => {
     
-    let Char1 = new char_Obj(1,"Saloth", "Saar", 45, "Male", "Dragonborn", "Sorcerer", 10)
-    let Char2 = new char_Obj(1,"Kaedwen", "Isaani", 36, "Male", "Dragonborn", "Sorcerer", 7)
-    console.log("\nChar1 table data:\n")
-    console.table(Char1)
+    let Char1 = new char_Obj(1,"Saloth", "Saar", 45, "Male", "Dragonborn", "Sorcerer", 10);
+    let Char2 = new char_Obj(2,"Kaedwen", "Isaani", 36, "Male", "Dragonborn", "Sorcerer", 8); 
+    let Char3 = new char_Obj(3,"Euridice", "Eurodollar", 23, "Female", "Human", "Bard", 5);
+    
+    //array to store characters
+    let ar_charBin = [];
+    
+    //push all chars into the array
+    ar_charBin.push(Char1, Char2, Char3);
+
+    //useState to store data for populating table
+    const [data, setData] = useState([]);
+
+    function barkCharData(){
+        console.log("barkCharData was fired! InputId was: tbd")
+    }
+
 
     return(
         <>
@@ -32,30 +43,32 @@ export const ExpTable = () => {
                         </th>
                     </tr>
                     </TableHead>
+
                     <tbody>
-                    <TableRow>
-                        <td>    
-                            {Char1.full_name}
-                        </td>
-
-                        <td>
-                            Level {Char1.level} {Char1.classType}
-                        </td>
-                    </TableRow>
-
-                    <TableRow>
-                        <td>    
-                            {Char2.full_name}
-                        </td>
-
-                        <td>
-                            Level {Char2.level} {Char2.classType}
-                        </td>
-                    </TableRow>
+                        {ar_charBin.map((character) => (
+                            <TableRow key={character.id} onclick="barkCharData()">
+                                <td>{character.full_name}</td>
+                                <td>Level {character.level} {character.classType}</td>
+                            </TableRow>
+                        ))}
                     </tbody>
                 </Table>
-            </Wrapper>
 
+                <Wrapper>
+                <h2>Object elements</h2>
+                <p>These fields should update with object<br/>attributes when the records above are clicked</p>
+
+                <input type="text" name="idField" placeholder="ID" disabled="true" />
+                <input type="text" name="fNameField" placeholder="First Name" disabled="true" />
+                <input type="text" name="sNameField" placeholder="Second Name" disabled="true" />
+                <input type="text" name="ageField" placeholder="Age" disabled="true" />
+                <input type="text" name="sNameField" placeholder="Second Name" disabled="true" />
+                <input type="text" name="genderField" placeholder="Gender" disabled="true" />
+                <input type="text" name="raceField" placeholder="Race" disabled="true" />
+                <input type="text" name="classField" placeholder="Class" disabled="true" />
+                <input type="text" name="levelField" placeholder="Level" disabled="true" />
+            </Wrapper>
+            </Wrapper>
         </>
     )
 }
