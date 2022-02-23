bf4d8536c74b429ba3652baec1636e515a4d5e39
Basil <sharpshot101@hotmail.co.uk>
2022-02-23 17:38:31 +0000

    Issues with websocket

--- src/Components/C-Websocket.js
+++ src/Components/C-Websocket.js
@@ -9,35 +9,39 @@ import {
     TextInput, 
     } from "../styles";
     
-    
+const Websocket = require("ws");
+
 //=====-----=====-----=====-----=====----→APP RENDERING HERE←-----=====-----=====-----=====-----=====
 export const Websock = () => {
 
     //create the websocket
-    // var socket = new Websocket("ws://localhost:5050");
-
+    const wss = new Websocket.Server({ port: 8082 })
+    
     // Connection opened
-    // socket.addEventListener('open', (event) => {
-    // socket.send('Hello Server!');
-    // });
+    wss.on("connection", ws => {
+        console.log("A new client has connected!");
+
+        //if connection is lost
+        ws.on("close", () => {
+            console.log("A client has disconnected");
+        });
+    });
 
     //get the text output so it can be changed
     var output = document.getElementById("wsConsole");
-
-
+    var rsOutput = document.getElementById("wsReadyState");
+    
     const connectClickHandler = () => {
-        output.value = "";
+        // output.value = "";
         console.log("'Connect' was clicked.");
         output.value = "'Connect' was clicked";
-        
     }
 
     const disconnectClickHandler = () => {
-        output.value = "";
+        // output.value = "";
         console.log("'Disconnect' was clicked.")
         output.value = "'Disconnect' was clicked";
     }
-
 
     return(
             <>
@@ -47,10 +51,12 @@ export const Websock = () => {
                     <Button inputWidth="120px" onClick={connectClickHandler}>Connect</Button>
                     <Button inputWidth="120px"onClick={disconnectClickHandler} >Disconnect</Button>
                     <RowSpacer/>
+                    <p>Console:</p>
                     <TextInput inputMargin="10px 0px 0px 0px" inputWidth="90%" id="wsConsole" placeholder="Idle" disabled={true}></TextInput>
+                    <p>Socket Readystate::</p>
+                    <TextInput inputMargin="10px 0px 0px 0px" inputWidth="90%" id="wsReadyState" placeholder="Idle" disabled={true}></TextInput>
                 </Wrapper>
             </Wrapper>
             </>
     )
-
 }
