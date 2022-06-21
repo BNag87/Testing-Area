298ae2451aa536716f5ef491b3da7238aa3b7196
Basil <sharpshot101@hotmail.co.uk>
2022-06-21 15:51:17 +0100

    Sidebar update. Rework

--- src/Components/Minor Components/C-Sidebar.js
+++ src/Components/Minor Components/C-Sidebar.js
@@ -1,8 +1,7 @@
 //==========----------→ ↓IMPORTS HERE↓ ←---------==========
-import React from 'react';
-
-import { Drawer, Box, Typography } from '@mui/material';
-
+import { React, useState } from 'react';
+import { Drawer, Box, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'
 //==========----------→ ↓STYLE IMPORTS HERE↓ ←---------==========
 import { Button } from "../../styles"
 
@@ -15,12 +14,27 @@ const toggleDrawer = () => {
 
 //==========----------→ ↓COMPONENT STARTS HERE↓ ←---------==========
 export const Sidebar = () => {
-return(
+    //state var to control the opening of the sidebar'
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
-    <Drawer anchor="left">
-        <Box p={2} width="250px" textAlign="center" role="presentation">
-            <Typography variant="h6" component="div">Side Panel</Typography>
-        </Box>
+//==========----------→ ↓COMPONENT PARTS HERE↓ ←---------==========
+return(
+<>
+    <IconButton size="large" edge="start" color="inherit" aria-label="logo" onClick={() => setIsDrawerOpen(true)}>
+        <MenuIcon>
+
+        </MenuIcon>
+    </IconButton>
+    <Drawer 
+        anchor="left" 
+        open={ isDrawerOpen }
+        onClose={() => setIsDrawerOpen(false)}
+    >
+                
+            <Box p={2} width="250px" textAlign="center" role="presentation">
+                <Typography variant="h6" component="div">Side Panel</Typography>
+            </Box>
     </Drawer>
+</>
 )
 }
