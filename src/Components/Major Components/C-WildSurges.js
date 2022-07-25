44ddaa1f77dbd13b6f12ed2ef5364069b9cbfee1
Basil Nagle <sharpshot101@hotmail.co.uk>
2022-07-25 17:45:21 +0100

    scsxds

--- src/Components/Major Components/C-WildSurges.js
+++ src/Components/Major Components/C-WildSurges.js
@@ -8,30 +8,23 @@ import {
     SuperTH,
     SuperTD,
     Button,
+    TextArea,
     TextInput,
+    H3,
     InvisiDiv,
     } from "../../styles";
 
-    import {  FirstPage, LastPage } from '@mui/icons-material';
-    import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-    import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 //==========----------→COMPONENT STARTS HERE
 export const WildSurges = () => {
 
 //==========----------→ ↓USESTATES HERE↓ ←---------==========
-    //useState to choose an effect array to map through for display
-    const [ST_ArrayMarker, set_ST_ArrayMarker] = useState(1);
+    // usestate to track random effects when generated
+    const [randEffect, setRandEffect] = useState("");
 
-//==========----------→ ↓GENERIC FUNCTIONS HERE↓ ←---------==========
-    //short version of console.log
-    const bark = (input) => {
-        console.log(input);
-    }
-
-    //create and return a random number between two inputs
-    const randRanged = (min, max) => { 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+        //short version of console.log
+        const bark = (input) => {
+            console.log(input);
+        }
 
 //==========----------→ ↓JSON IMPORTS/CONVERSIONS HERE↓ ←---------==========
     //get the json data
@@ -81,14 +74,16 @@ export const WildSurges = () => {
         AR_pag11, AR_pag12, AR_pag13, AR_pag14, AR_pag15, AR_pag16, AR_pag17, AR_pag18, AR_pag19, AR_pag20
         );
 
-                       
-//FUNCTIONS BLOCK===============================================================
+    //function to get a random number between to specific numbers
+    const randRanged = (min, max) => { 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
 
-// get random magical effect (plus ID number)
+    //function to grab and spit out a random spell effect
     const getRecord = (input) => {
     
         //find the right array first.
-        //each one contains 500, so divide input by 500
+        //each one contains 500, soo divide input by 500
 
         //input will be the item index. use that to pick the right array
         let index = Math.floor(input/500);
@@ -114,9 +109,10 @@ export const WildSurges = () => {
             bark("\n===============");
         }
         return value;
+
     }
 
-//display random magical effect
+    //onclick function, updayes display fields with random effect/id
     const randomEffect = () => {
         //Internal variables
         let var_TextOut = document.getElementById("effectTextBox");
@@ -130,75 +126,10 @@ export const WildSurges = () => {
                 var_TextOut.value = getRecord(var_idData);
             }
         catch(error){
-            bark("Bowel explosion in click:");
+            bark("Bowel explosion in onclick function:");
             bark(error);
         }
     }
-
-//ONCLICK FUNCTIONS BLOCK===============================================================
-    //used to change the 'page number' of a map block to display records        
-const setPage = (input) => {
-    try{
-    let pos = ST_ArrayMarker;
-
-    switch(input)
-    {
-        case "next":            
-            pos++;
-            set_ST_ArrayMarker(pos);
-
-            if((pos) >= 21)
-            {
-                pos=20;
-                set_ST_ArrayMarker(pos);
-            }  
-            else if((pos) <= 1)
-            {
-                pos=1;
-                set_ST_ArrayMarker(pos);
-            }
-
-            break;
-
-        case "last":
-            pos=20;
-            set_ST_ArrayMarker(pos);
-            break;
-
-        case "prev":
-            pos--;
-            set_ST_ArrayMarker(pos);
-            
-            if((pos) >= 21)
-            {
-                pos=20;
-                set_ST_ArrayMarker(pos);
-            }  
-            else if((pos) <= 1)
-            {
-                pos=1;
-                set_ST_ArrayMarker(pos);
-            }
-
-            break;
-
-        case "first":
-            pos=1;
-            set_ST_ArrayMarker(pos);
-            break;
-        default:
-            break;
-    }
-}
-    
-    catch(e)
-    {
-        bark("Bowel obstruction in setPage function:")
-        bark(e)
-    }
-    }
-
-//COMPONENT RETURN BLOCK===============================================================
 
     return(
         <>
@@ -218,7 +149,7 @@ const setPage = (input) => {
                             {/* TABLEROW TAG */}
                             <SuperTH colSpan={4}>
                                 <h2>Random Spell Failures</h2>
-                                Click the '?' button to find a random spell effect. These can be used as Wild magic surges or just for random spell effects as needed by a DM.
+                                Click the '?' button or type in the result of a d10000 roll to find a random spell effect. These can be used as Wild magic surges or just for random spell effects as needed by a DM.
                             <hr/>  
 
                             <InvisiDiv inputMargin="15px 0px 0px 10px">
@@ -246,14 +177,12 @@ const setPage = (input) => {
                                 
                                 <Button 
                                     inputBackground = "#222222" 
-                                    inputRadius = "0px 8px 8px 0px"
+                                    inputBorder= "outset rgba(150, 40, 40, 0.4) 3px" 
                                     inputColor = "#cccccc" 
-                                    inputMargin= "0px 10px 8px 0px"
                                     inputFontSize = "normal" 
                                     inputFontVariant = "normal" 
-                                    inputWidth="50px" 
-                                    inputHeight="64px" 
-                                    onClick = {() => randomEffect()}>
+                                    inputWidth = "40px" 
+                                    onClick = {() => randomEffect(0)}>
                                     ?
                                 </Button>
                             </InvisiDiv>
@@ -263,86 +192,22 @@ const setPage = (input) => {
                             </SuperTH>
                         </TableRow>
                                 
-{/* ARRAY MAPPING! FIRST MAP===============================================================*/}
+                {/* ARRAY MAPPING! FIRST MAP===============================================================*/}
+                    {/* {
+                        objInnerValues.map((thing) => (
+                            <>
+                            <TableRow NoHoverTR>
+                            <SuperTH>{thing.id}</SuperTH>
+                            <SuperTD NoHoverTD>{thing.text}</SuperTD>
+                            </TableRow>
+                            </>
+                        ))
+                    } */}
 
-    {AR_concSplits[(ST_ArrayMarker-1)].map((thing, index, elements) => (
-    (index % 2 === 0) ?
-    <> 
-         
-
-            <TableRow NoHoverTR inputHeight="20px" inputBackgroundColour="gray" inputFontColour="black">
-            
-            <SuperTH>
-                {elements[index].id}
-            </SuperTH>
-            
-            <SuperTD NoHoverTD inputBackground="black">
-                {elements[index].text}
-            </SuperTD>
-
-            <SuperTH>
-                {elements[index+1]}
-            </SuperTH>
-
-            <SuperTD NoHoverTD inputBackground="black">
-                {elements[index+1]}
-            </SuperTD>
-
-            </TableRow>
-    </>
-    :
-    <>
-        {bark('Index was odd. Skipping to next')}
-    </>
-        ))} 
-
-
-
-                <TableRow>
-                    <SuperTH colSpan={4}>
-                    <InvisiDiv 
-                        inputWidth="auto" 
-                        inputBackground="none"
-                        inputBorder="none"
-                        inputFlexDirection="row"
-                        >
-                    {/* PAGINATION FEATURE */}
-
-                        <InvisiDiv 
-                            inputWidth="auto" 
-                            inputBackground="rgba(255, 255, 255, 0.1)" 
-                            inputBorder="solid rgba(255,255,255,0.2) 2px"
-                            inputRadius="100px"
-                            inputMargin="10px 0px 10px 0px">
-
-                            <Button PageButton onClick={() => setPage("first")}> 
-                                <FirstPage/> 
-                            </Button>
-                            <Button PageButton onClick={() => setPage("prev")}> 
-                                <NavigateBeforeIcon/>
-                            </Button>
-                                    
-                            <TextInput
-                            inputBackgroundColour="none"
-                            inputBorder="none"
-                            inputColour="white"
-                            inputPadding="3px"
-                            inputMargin="5px"
-                            value={"Page "+ST_ArrayMarker+"/20"}/>
-                            
-                            <Button PageButton onClick={() => setPage("next")}>
-                                <NavigateNextIcon/>
-                            </Button>
-                            <Button PageButton onClick={() => setPage("last")}> 
-                                <LastPage/>
-                            </Button>
-                        </InvisiDiv>
-
-                    </InvisiDiv>    
-                    </SuperTH>
-
-
-                </TableRow>    
+                    <SuperTD NoHoverTD colSpan={2}>
+                    
+                    </SuperTD>
+                    
                     </TableHead>
                 </Table>
             </Wrapper>
